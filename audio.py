f93fa1813a9ab649e7f763db9e31996f293aef54
I518046 <konrad.schewe@sap.com>
2024-03-24 15:33:00 +0100

    remove env

--- audio.py
+++ audio.py
@@ -1,17 +1,53 @@
+import os
+import re
+
 import numpy as np
 import whisper
+from pyannote.audio import Pipeline
 from pydub import AudioSegment
 
+
+def millisec(timeStr):
+    spl = timeStr.split(":")
+    s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
+    return s
+
 model = whisper.load_model("medium")
+pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization', use_auth_token=os.environ['HUGGINGFACE_TOKEN'])
 
 
 class Audio:
     def __init__(self, path):
         self.path = path
-    
+        self.audio = AudioSegment.from_file(self.path, "pcm", sample_width=16, frame_rate=16000, channels=1)
+
     def transcribe(self):
-        audio = AudioSegment.from_file(self.path, "pcm", sample_width=16, frame_rate=16000, channels=1)
-        input = np.frombuffer(audio.raw_data, np.int16).flatten().astype(np.float32) / 32768.0
+        input = np.frombuffer(self.audio.raw_data, np.int16).flatten().astype(np.float32) / 32768.0
 
         transcription = model.transcribe(input)
         return transcription
+    
+    def diarize_speaker(self):
+        dzs = pipeline(self.path)
+
+        # Grouping
+        groups = []
+        g = []
+        lastend = 0
+
+        for d in dzs:   
+            if g and (g[0].split()[-1] != d.split()[-1]):      #same speaker
+                groups.append(g)
+                g = []
+        
+        g.append(d)
+        
+        end = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=d)[1]
+        end = millisec(end)
+        if lastend > end:       #segment engulfed by a previous segment
+            groups.append(g)
+            g = [] 
+        else:
+            lastend = end
+        if g:
+            groups.append(g)
--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+import re
 from os import remove
 from os.path import dirname, join, realpath
 from uuid import uuid4
@@ -5,7 +6,7 @@ from uuid import uuid4
 from flask import request
 from flask_restful import Resource
 
-from audio import Audio
+from audio import Audio, millisec
 
 
 class TranscriptionController(Resource):
@@ -16,15 +17,44 @@ class TranscriptionController(Resource):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in self.ALLOWED_EXTENSIONS
     
     def post(self):
+        def transcribe(path):
+            audio = Audio(path)
+            transcription = audio.transcribe()
+            return transcription
+        def transcribe_with_speaker(path):
+            audio = Audio(path)
+            sections = audio.diarize_speaker()
+            gidx = -1
+            response = []
+            for s in sections:
+                start = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=s[0])[0]
+                end = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=s[-1])[1]
+                start = millisec(start) #- spacermilli
+                end = millisec(end)  #- spacermilli
+                print(start, end)
+                gidx += 1
+                base_path = path.split('.pcm')[0]
+                section_path = f'{base_path}-{gidx}.pcm'
+                audio[start:end].export(section_path, format='pcm')
+                section_audio = Audio(section_path)
+                transcription = section_audio.transcribe()
+                response.append({
+                    'speaker': s[0].split()[-1],
+                    'transcription': transcription
+                })
+            return sections
+
         file = request.files['audio']
+        features = request.form['features'].replace(' ', '').split(',')
         if file and self.allowed_file(file.filename):
             filename = f'{uuid4()}.pcm'
             path = join(self.UPLOAD_FOLDER, filename)
             file.save(path)
             try:
-                audio = Audio(path)
-                transcription = audio.transcribe()
-                return transcription
+                if 'speaker-diarization' in features:
+                    return transcribe_with_speaker(path)
+                else:
+                    return transcribe(path)
             except Exception as e:
                 print(e)
                 pass
